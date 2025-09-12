09598057a592df31de6df36a204180c7027f1225
tr4pp1st <bernd.hellerich@gmx.de>
2025-09-12 20:49:00 +0200

    Add Day 1 Project: Band Name Generator

---
+++ 001_band_name_generator/main.py
@@ -0,0 +1,7 @@
+# 100 Days of Code: Python
+# Day 1 Project: Band Name Generator
+
+print("Welcome to the Band Name Generator.")
+city = input("Enter the name of the city you grew up:\n").strip()
+pet = input("Enter the name of your pet:\n").strip()
+print(f"Your band name could be: \"{city.title()} {pet.title()}\"")
